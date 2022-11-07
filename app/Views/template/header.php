17e24ea5f18d085c0b9fe3bd8019c234d02c70a2
strelok <alexanderivanov416@gmail.com>
2022-11-07 17:29:12 +0800

    home

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -1,32 +1,59 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200 bg-white">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <a href="https://flowbite.com" class="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap text-gray-900">Flowbite</span>
         </a>
         <div class="flex items-center">
-            <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                    <li>
+                        <a href="#" class="text-gray-900 hover:underline" aria-current="page">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 hover:underline">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 hover:underline">Testimony</a>
+                    </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2">
+        <a href="https://flowbite.com" class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap text-gray-900">&nbsp;</span>
+        </a>
+        <div class="flex items-center">
+            <a href="#" class="mr-6 text-sm font-medium text-gray-500 text-gray-900 hover:underline">For last minute booking please contact us via <br> Whatsapp: +6287 8629 83711</a>
         </div>
     </div>
 </nav>
-<nav class="bg-gray-50 dark:bg-gray-700">
+<nav class="bg-gray-50 dark:bg-gray-900">
     <div class="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
         <div class="flex items-center">
             <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Bali Tours</a>
                 </li>
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Bali Half Day Tours</a>
                 </li>
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Bali Full Day Tours</a>
                 </li>
                 <li>
-                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Bali Tour Packages</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Bali Round Trip</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Bali Honeymoon Packages</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+<div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6">
+    <a href="#">Bali Tour</a>
+</div>
