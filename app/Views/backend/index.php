9ec90b923469a4f67db92f487e2bc4c341efd83f
strelok <alexanderivanov416@gmail.com>
2022-11-10 11:58:42 +0800

    tes

--- app/Views/backend/index.php
+++ app/Views/backend/index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({selector:'textarea'});</script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tailwind Admin Template</title>
