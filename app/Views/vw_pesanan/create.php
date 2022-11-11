4e4157bd9978186c4144fc3badc1b65f37902451
ekaari <ekaari1256@gmail.com>
2022-11-11 14:13:42 +0800

    update

--- app/Views/vw_pesanan/create.php
+++ app/Views/vw_pesanan/create.php
@@ -2,10 +2,8 @@
 
 <?= $this->Section('content'); ?>
 <!-- Begin Page Content -->
-<div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Form Tambah Data Pesanan</h1>
     <?php if (!empty(session()->getFlashdata('error'))) : ?>
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <h4>Periksa Entrian Form</h4>
@@ -16,42 +14,44 @@
             </button>
         </div>
     <?php endif; ?>
-    <form method="POST" action="<?= base_url('/pesanan/create') ?>">
+
+<div class="w-full lg:w-full my-6 pr-0 lg:pr-2">
+    <p class="text-xl pb-6 flex items-center">
+         <i class="fas fa-list mr-3"></i> Form Tambah Data Pesanan
+    </p>
+    <div class="leading-loose">
+        <form class="w-full p-10 bg-white rounded shadow-xl" method="POST" action="<?= base_url('/pesanan/create') ?>">
         <?= csrf_field() ?>
-        <div class="row mb-3">
-            <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nama Customer</label>
-            <input type="text" name="nama" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="nama" placeholder="Nama Customer" value="<?= old('nama'); ?>">
-        </div>
-        <div class="row mb-3">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-            <input type="email" id="email" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com">
-        </div>
-        <div class="row mb-3">
-            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Pilih Paket Tour</label>
-            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="" hidden></option>
-                <?php foreach ($paket as $row) { ?>
-                    <option value="<?= $row->id_paket ?>"><?= $row->nama_paket ?></option>
-                <?php  } ?>
-            </select>
-        </div>
-</div>
-<div class="row mb-3">
-    <label for="nama_hotel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nama Hotel</label>
-    <input type="text" name="nama_hotel" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="nama_hotel" placeholder="Nama Hotel" value="<?= old('nama_hotel'); ?>">
-</div>
-<div class="row mb-3">
-    
-<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." value="<?= old('email'); ?>"></textarea>
-
-</div>
-
-
-    <div>
-        <button type=" submit" class="btn btn-primary">Simpan</button>
-    </form>
-
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="name">Nama Customer</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="name" name="name" type="text" required="" id="nama" placeholder="Nama Customer" value="<?= old('nama'); ?>">
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="email">Email</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="text" required="" id="email" placeholder="Email Customer" value="<?= old('email'); ?>">
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="email">Pilih Paket Tour</label>
+                <select class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"" id="email" name="email" type="text" required="" placeholder="Your Email" aria-label="Email">
+                    <option value="" hidden></option>
+                    <?php foreach ($paket as $row) { ?>
+                        <option value="<?= $row->id_paket ?>"><?= $row->nama_paket ?></option>
+                    <?php  } ?>
+                </select>
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="nama_hotel">Nama Hotel</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="nama_hotel" name="nama_hotel" type="text" required="" id="nama_hotel" placeholder="Nama Hotel" value="<?= old('nama_hotel'); ?>">
+            </div>
+            <div class="mt-2">
+                <label class=" block text-sm text-gray-600" for="message">Message</label>
+                <textarea class="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded" id="pesan" name="pesan" rows="6" placeholder="Your inquiry.." value="<?= old('pesan'); ?>"></textarea>
+                </div>
+                <div class="mt-6">
+                    <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">Simpan</button>
+                </div>
+        </form>
+    </div>
 </div>
 
 <?= $this->endSection(); ?>
--- app/Views/vw_pesanan/edit.php
+++ app/Views/vw_pesanan/edit.php
@@ -2,7 +2,6 @@
 
 <?= $this->Section('content'); ?>
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Form Ubah Data Pesanan</h1>
 <?php if (!empty(session()->getFlashdata('error'))) : ?>
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <h4>Periksa Entrian Form</h4>
@@ -13,46 +12,43 @@
         </button>
     </div>
 <?php endif; ?>
-<form method="POST" action="<?= base_url('/pesanan/update') ?>" value="patch">
-    <?= csrf_field() ?>
-    <div class="row mb-3">
-        <label for="nama_customer" class="col-sm-2 col-form-label">Nama Customer</label>
-        <div class="col-sm-10">
-            <input type="text" name="nama" class="form-control" id="nama" value="<?= $pesanan->nama; ?>">
-        </div>
+<div class="w-full lg:w-full my-6 pr-0 lg:pr-2">
+    <p class="text-xl pb-6 flex items-center">
+         <i class="fas fa-list mr-3"></i> Form Ubah Data Pesanan
+    </p>
+    <div class="leading-loose">
+        <form class="w-full p-10 bg-white rounded shadow-xl" method="POST" action="<?= base_url('/pesanan/edit') ?>">
+        <?= csrf_field() ?>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="name">Nama Customer</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="name" name="name" type="text" required="" id="nama" value="<?= $pesanan->nama; ?>">
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="email">Email</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="email" name="email" type="text" required="" id="email" value="<?= $pesanan->email; ?>">
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="email">Pilih Paket Tour</label>
+                <select class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"" id="email" name="email" type="text" required="" placeholder="Your Email">
+                    <option value="" hidden></option>
+                    <?php foreach($paket as $row) { ?>
+                    <option value="<?= $row->id_paket ?>" <?= $pesanan->id_paket == $row->id_paket ? "selected" : "";?>><?= $row->nama_paket ?></option>
+                    <?php  } ?>
+                </select>
+            </div>
+            <div class="mt-2">
+                <label class="block text-sm text-gray-600" for="nama_hotel">Nama Hotel</label>
+                <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="nama_hotel" name="nama_hotel" type="text" required="" id="nama_hotel" value="<?= $pesanan->nama_hotel; ?>">
+            </div>
+            <div class="mt-2">
+                <label class=" block text-sm text-gray-600" for="message">Message</label>
+                <textarea class="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded" id="pesan" name="pesan" rows="6" value="<?= $pesanan->pesan; ?>"></textarea>
+                </div>
+                <div class="mt-6">
+                    <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">Simpan</button>
+                </div>
+        </form>
     </div>
-    <div class="row mb-3">
-        <label for="email" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-            <input type="text" name="email" class="form-control" id="email" value="<?= $pesanan->email; ?>">
-        </div>
-    </div>
-    <div class="row mb-3">
-        <label for="id_paket" class="col-sm-2 col-form-label">Paket Tour</label>
-        <div class="col-sm-10">
-            <select name="id_paket" class="form-control" id="id_paket">
-                <option value="" hidden></option>
-                <?php foreach ($paket as $row) { ?>
-                    <option value="<?= $row->id_paket ?>" <?= $pesanan->id_paket == $row->id_paket ? "selected" : ""; ?>><?= $row->nama_paket ?></option>
-                <?php  } ?>
-            </select>
-        </div>
-    </div>
-    <div class="row mb-3">
-        <label for="nama_hotel" class="col-sm-2 col-form-label">Nama Hotel</label>
-        <div class="col-sm-10">
-            <input type="text" name="nama_hotel" class="form-control" id="nama_hotel" value="<?= $pesanan->nama_hotel; ?>">
-        </div>
-    </div>
-    <div class="row mb-3">
-        <label for="pesan" class="col-sm-2 col-form-label">Massage</label>
-        <div class="col-sm-10">
-            <input type="text" name="pesan" class="form-control" id="pesan" value="<?= $pesanan->pesan; ?>">
-        </div>
-    </div>
+</div>
 
-
-    <br>
-    <button type="submit" class="btn btn-primary">Simpan</button>
-</form>
 <?= $this->endSection(); ?>
