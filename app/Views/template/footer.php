2545aef7b4a7d32d17b29f38e8238dc9300fa1aa
strelok <alexanderivanov416@gmail.com>
2022-11-14 17:23:31 +0800

    footer

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,6 +1,8 @@
 
-<footer class="p-9 mx-auto bg-white shadow md:flex md:items-center md:justify-between md:p-6">
-    <ul class="flex flex-wrap items-center mx-9 max-w-screen-xl text-sm text-gray-900 dark:text-gray-900 sm:mt-0 px-8 border">
+<footer class="">
+    <ul class="inline-block items-center mx-auto max-w-screen-xl md:text-xs sm:text-sm text-gray-900 dark:text-gray-900 sm:mt-0 px-4">
+    <div class="flex flex-wrap border px-2">
+    <div class="flex flex-row">
         <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">Home</a>
         </li>
@@ -14,15 +16,6 @@
             <a href="#" class="mr-4 hover:underline md:mr-6 ">Terms and Condition</a>
         </li>
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Bali Car Charter</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Bali Spa Packages</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Jimbaran Seafood Dinner</a>
-        </li>
-        <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">Bali Places of Interest</a>
         </li>
         <li>
@@ -31,8 +24,11 @@
         <li>
             <a href="#" class="mr-4 hover:underline md:mr-6">Site Map</a>
         </li>
+    </div>
         <p>Address : Lorem Ipsum | Phone or Whatsapp : +6211 1111 1111 | email : lorem@ipsum.com| website : www.lipsum.com
             <br>lipsum is managed by pt. lipsum | license no : 666.66/77777/XX/ABCD - copyright © 2077 - all rights reserved</p>
+        
+        </div>
     </ul>
     </div>
 </footer>
