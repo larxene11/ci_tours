73aa75fd6499815c0d57203a6206dd8325523495
strelok <alexanderivanov416@gmail.com>
2022-11-22 14:44:28 +0800

    inline2

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,32 +1,33 @@
 
 <footer class="">
-    <ul class="md:inline-block items-center mx-auto max-w-screen-xl md:text-xs sm:text-sm text-gray-900 dark:text-gray-900 sm:mt-0 px-4">
-    <div class="flex flex-wrap border px-2">
-    <div class="flex flex-row">
-        <li>
-            <a href="<?= base_url('/') ?>" class="mr-4 hover:underline md:mr-6 ">Home</a>
-        </li>
-        <li>
-            <a href="<?= base_url('/about') ?>" class="mr-4 hover:underline md:mr-6">About Us</a>
-        </li>
-        <li>
-            <a href="<?= base_url('/contact') ?>" class="mr-4 hover:underline md:mr-6">Contact Us</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">Terms and Condition</a>
-        </li>
-        <li>
-            <a href="<?= base_url('/testimony') ?>" class="mr-4 hover:underline md:mr-6">Testimony</a>
-        </li>
-        <li>
-            <a href="<?= base_url('/sitemap') ?>" class="mr-4 hover:underline md:mr-6">Site Map</a>
-        </li>
-        <br>
-    </div>
+    <ul class="items-center mx-auto max-w-screen-xl md:text-xs sm:text-sm text-gray-900 dark:text-gray-900 sm:mt-0 px-4">
+    <div class="flex flex-wrap border px-2 grid-rows-2">
+        <div class="flex flex-row">
+            <li>
+                <a href="<?= base_url('/') ?>" class="mr-4 hover:underline md:mr-6 ">Home</a>
+            </li>
+            <li>
+                <a href="<?= base_url('/about') ?>" class="mr-4 hover:underline md:mr-6">About Us</a>
+            </li>
+            <li>
+                <a href="<?= base_url('/contact') ?>" class="mr-4 hover:underline md:mr-6">Contact Us</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Terms and Condition</a>
+            </li>
+            <li>
+                <a href="<?= base_url('/testimony') ?>" class="mr-4 hover:underline md:mr-6">Testimony</a>
+            </li>
+            <li>
+                <a href="<?= base_url('/sitemap') ?>" class="mr-4 hover:underline md:mr-6">Site Map</a>
+            </li>
+            <br>
+        </div>
         <p>Address : Lorem Ipsum | Phone or Whatsapp : +6211 1111 1111 | email : lorem@ipsum.com| website : www.lipsum.com
             <br>lipsum is managed by pt. lipsum | license no : 666.66/77777/XX/ABCD - copyright © 2077 - all rights reserved</p>
         
         </div>
     </ul>
     </div>
+    <br><br>
 </footer>
